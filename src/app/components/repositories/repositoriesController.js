5c581e30f08c66e1714a4cea91e4bfce5bdf81b6
Thai Nguyen <thai.nguyen@eastagile.com>
2015-05-04 15:05:17 +0700

    Add repositories index page and reload button

---
+++ src/app/components/repositories/repositoriesController.js
@@ -0,0 +1,10 @@
+githubStats.controller('repositoriesController',
+  ['$scope', 'repositoriesResource', function($scope, Repository) {
+    var init = function() {
+      Repository.query(function(data) {
+        $scope.repositories = data;
+      });
+    }
+    init();
+  }
+]);
---
+++ src/app/shared/directives/reloadButton.js
@@ -0,0 +1,10 @@
+githubStats.directive('reloadButton', ['$state', function($state) {
+  return {
+    restrict: 'C',
+    link: function(scope, element) {
+      element.on('click', function() {
+        $state.go($state.current, {}, {reload: true});
+      });
+    }
+  }
+}]);
